bad47d7932d2d8d4491362f474b2e2be72611745
superelise <57939154+EliseBrenna@users.noreply.github.com>
2020-02-20 10:25:28 +0100

    edited case sensitive img

--- src/components/Work.js
+++ src/components/Work.js
@@ -14,10 +14,10 @@ class Work extends React.Component {
                     </nav>
                 </div>
                 <div className="work-gallery">
-                    <a className="digipant" href="https://github.com/EliseBrenna/pante-app" target="_blank"><img src="./digipant.png" alt="digipant"/></a>
-                    <a className="job" href="https://github.com/EliseBrenna/job" target="_blank"><img src="./job.png" alt="jobbrowser"/></a>
-                    <a className="eggxact" href="https://github.com/EliseBrenna/eggxact" target="_blank"><img src="./eggxact.png" alt="eggxact"/></a>
-                    <a className="twitter" href="https://github.com/EliseBrenna/academy-twitter-clone" target="_blank"><img src="./twitter.png" alt="twitter"/></a>
+                    <a className="digipant" href="https://github.com/EliseBrenna/pante-app" target="_blank"><img src="./digipant.PNG" alt="digipant"/></a>
+                    <a className="job" href="https://github.com/EliseBrenna/job" target="_blank"><img src="./job.PNG" alt="jobbrowser"/></a>
+                    <a className="eggxact" href="https://github.com/EliseBrenna/eggxact" target="_blank"><img src="./eggxact.PNG" alt="eggxact"/></a>
+                    <a className="twitter" href="https://github.com/EliseBrenna/academy-twitter-clone" target="_blank"><img src="./twitter.PNG" alt="twitter"/></a>
                 </div>
             </div>
         )
