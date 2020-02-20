114db5d1c93b9002e985a4867261e9d5253f04fd
superelise <57939154+EliseBrenna@users.noreply.github.com>
2020-02-20 10:20:36 +0100

    Updates

---
+++ precache-manifest.252bf7e89ac9591c8e5ed30d56877b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d60659141bcaa85363503e79a107777",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "1fb5173e2da3cfb81c7e",
+    "url": "/portofolio/static/css/main.bc0b59ec.chunk.css"
+  },
+  {
+    "revision": "9fbecea10edf0abaf6ae",
+    "url": "/portofolio/static/js/2.7da0912c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portofolio/static/js/2.7da0912c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb5173e2da3cfb81c7e",
+    "url": "/portofolio/static/js/main.3b2f3054.chunk.js"
+  },
+  {
+    "revision": "19d1097e3554bac1aab3",
+    "url": "/portofolio/static/js/runtime-main.0168f7f7.js"
+  }
+]);
