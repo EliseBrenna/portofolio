d6a2e7e4adc29d05b5ca22506f9dced1a8691d6f
superelise <57939154+EliseBrenna@users.noreply.github.com>
2020-03-05 09:55:36 +0100

    added shoppingcart project

--- src/components/Work.js
+++ src/components/Work.js
@@ -14,10 +14,21 @@ class Work extends React.Component {
                     </nav>
                 </div>
                 <div className="work-gallery">
-                    <a className="digipant" href="https://github.com/EliseBrenna/pante-app" target="_blank"><img src="./digipant.PNG" alt="digipant"/></a>
-                    <a className="job" href="https://github.com/EliseBrenna/job" target="_blank"><img src="./job.PNG" alt="jobbrowser"/></a>
-                    <a className="eggxact" href="https://github.com/EliseBrenna/eggxact" target="_blank"><img src="./eggxact.PNG" alt="eggxact"/></a>
-                    <a className="twitter" href="https://github.com/EliseBrenna/academy-twitter-clone" target="_blank"><img src="./twitter.PNG" alt="twitter"/></a>
+                    <a className="digipant" href="https://github.com/EliseBrenna/pante-app" target="_blank">
+                        <img src={require("../digipant.PNG")} alt="digipant"/>
+                    </a>
+                    <a className="job" href="https://github.com/EliseBrenna/job" target="_blank">
+                        <img src={require("../job.PNG")} alt="jobbrowser"/>
+                    </a>
+                    <a className="eggxact" href="https://github.com/EliseBrenna/eggxact" target="_blank">
+                        <img src={require("../eggxact.PNG")} alt="eggxact"/>
+                    </a>
+                    <a className="twitter" href="https://github.com/EliseBrenna/academy-twitter-clone" target="_blank">
+                        <img src={require("../twitter.PNG")} alt="twitter"/>
+                    </a>
+                    <a className="shoppingcart" href="https://github.com/EliseBrenna/phone-store" target="_blank">
+                        <img src={require("../shoppingcart.PNG")} alt="shoppingcart"/>
+                    </a>
                 </div>
             </div>
         )
